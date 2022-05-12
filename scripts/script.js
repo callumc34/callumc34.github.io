1eeb1f993ac54a7d094e201d4a869e0f7ca1eefa
Callum C <callumc34@hotmail.com>
2022-05-12 16:51:07 +1200

    feat: Converted scripting to jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,59 @@
-const scrollDistance = 80;
+const scrollHeaderDistance = 100;
+const scrollHomeDistance = 400;
+
+function checkScroll(distance) {
+	if ($(window).scrollTop() >= distance) return true;
+	return false;
+}
 
 function changeHeaderColour(e) {
-	if (window.scrollY >= scrollDistance || window.pageYOffset >= scrollDistance) {
-		document.getElementById("header").className = "scrolled";
-		document.getElementById("header-placeholder").style.display = "block";
+	if (checkScroll(scrollHeaderDistance)) {
+		$(header).addClass("scrolled");
+		$("#header-placeholder").css({display: "block"});
 	}
 	else {
-		document.getElementById("header").className = "";
-		document.getElementById("header-placeholder").style.display = "none";
+		$(header).removeClass("scrolled");
+		$("#header-placeholder").css({display: "none"});
+	}
+}
+
+function transitionSections(e) {
+	if (checkScroll(scrollHomeDistance) && $(home).is(":visible")) {
+		$(home).transition({
+			animation: "fade up",
+			duration: "1000ms",
+			queue: false,
+			onComplete: transitionSections
+		});
+	} else if (!checkScroll(scrollHomeDistance) && !$(home).is(":visible")) {
+		$(home).transition({
+			animation: "fade up",
+			duration: "1000ms",
+			queue: false
+		});
 	}
 }
 
 function switchTab(e) {
 	e.preventDefault();
+		
+	if ($(this).hasClass('tabs-item-active')) return false;
 
-	if (this.className.includes("tabs-item-active")) return false;
-
-	document.getElementsByClassName("tabs-item-active")[0].className = "tabs-item-link";
-	this.className = "tabs-item-link tabs-item-active";
+	$('.tabs-item-active').removeClass('tabs-item-active');
+	$(this).addClass('tabs-item-active');
 }
 
-window.onscroll = changeHeaderColour;
+function handleScroll(e) {
+	changeHeaderColour(e);
+	transitionSections(e);
+}
 
-for (let tab of document.getElementsByClassName("tabs-item-link")) tab.onclick = switchTab;
+$(document).ready(function() {
+	$(window).scroll(handleScroll);
+	$(".tabs-item-link").click(switchTab);
+	$(home).transition({
+		animation: "fade up",
+		duration: "1000ms",
+		queue: false
+	});
+});
